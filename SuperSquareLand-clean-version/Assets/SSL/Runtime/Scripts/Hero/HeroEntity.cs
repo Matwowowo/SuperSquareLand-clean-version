46b652f16507eb285ce735e381c8ab4e669c74ca
Mathis Bancquart <2012mathis2@gmail.com>
2024-04-29 10:35:48 +0200

    Orientation

--- SuperSquareLand-clean-version/Assets/SSL/Runtime/Scripts/Hero/HeroEntity.cs
+++ SuperSquareLand-clean-version/Assets/SSL/Runtime/Scripts/Hero/HeroEntity.cs
@@ -11,6 +11,7 @@ public class HeroEntity : MonoBehaviour
 
     [Header("Orientation")]
     [SerializeField] private Transform _orientVisualRoot;
+    private float _orientX = 1f;
 
     [Header("Debug")]
     [SerializeField] private bool _guiDebug = false;
@@ -22,9 +23,15 @@ public class HeroEntity : MonoBehaviour
 
     private void FixedUpdate()
     {
+        _ChangeOrientFromHorizontalMovement();
         _ApplyHorizontalSpeed();
     }
 
+    private void _ChangeOrientFromHorizontalMovement()
+    {
+        if (_moveDirX == 0f) return;
+        _orientX = Mathf.Sign(_moveDirX);
+    }
     private void _ApplyHorizontalSpeed()
     {
         Vector2 velocity = _rigidbody.velocity;
@@ -39,6 +46,12 @@ public class HeroEntity : MonoBehaviour
 
     private void _UpdateOrientVisual()
     {
+        
+        Vector3 newScale = _orientVisualRoot.localScale;
+        newScale.x = _orientX;
+        _orientVisualRoot.localScale = newScale;    
+    
+    
     }
 
     private void OnGUI()
@@ -48,6 +61,7 @@ public class HeroEntity : MonoBehaviour
         GUILayout.BeginVertical(GUI.skin.box);
         GUILayout.Label(gameObject.name);
         GUILayout.Label($"MoveDirX = {_moveDirX}");
+        GUILayout.Label($"OrientX = {_orientX}");
         GUILayout.Label($"Horizontal Speed = {_horizontalSpeed}");
         GUILayout.EndVertical();
     }
