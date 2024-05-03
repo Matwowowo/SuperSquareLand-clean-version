9ac736656ece6205a95002b86becdcc70e2414bb
Mathis Bancquart <2012mathis2@gmail.com>
2024-05-03 10:48:20 +0200

    Follow Character

--- SuperSquareLand-clean-version/Assets/SSL/Runtime/Scripts/Camera/CameraProfile.cs
+++ SuperSquareLand-clean-version/Assets/SSL/Runtime/Scripts/Camera/CameraProfile.cs
@@ -2,12 +2,28 @@
 
 public class CameraProfile : MonoBehaviour
 {
+    [Header("Type")]
+    [SerializeField] private CameraProfileType _profileType = CameraProfileType.Static;
+
+    [Header("Follow")]
+    [SerializeField] private Transform _targetToFollow;
+    
+    public CameraProfileType ProfileType => _profileType;
+
+    public Transform TargetToFollow => _targetToFollow;
+    
     private Camera _camera;
 
     public float CameraSize => _camera.orthographicSize;
 
     public Vector3 Position => _camera.transform.position;
 
+    public enum CameraProfileType
+    {
+        Static = 0,
+        FollowTarget
+    }
+    
     private void Awake()
     {
         _camera = GetComponent<Camera>();
